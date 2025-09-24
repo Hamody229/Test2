b6c9563b895d7c20bb820fd8a65e1d430f192fd7
Hamody <ahmedeslam229200@gmail.com>
2025-09-24 23:54:09 +0300

    modified test.py with publisher

--- test.py
+++ test.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
- 
- 
-class MyCustomNode(Node): # MODIFY NAME
+from std_msgs.msg import String  #MODIFY TYPE
+
+class PublisherNode(Node): #MODIFY NAME
     def __init__(self):
-        super().__init__("node_name") # MODIFY NAME
- 
- 
+        super().__init__('minimal_publisher')
+
+        self.publisher_ = self.create_publisher(String, 'topic', 10) # MODIFY TOPIC AND TYPE
+
+        timer_period = 1.0  #MODIFY TIMER RATE 
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Hello ROS2: {self.i}'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+        self.i += 1
+
 def main(args=None):
     rclpy.init(args=args)
-    node = MyCustomNode() # MODIFY NAME
+    node = PublisherNode()
     rclpy.spin(node)
     rclpy.shutdown()
- 
- 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
