9ddce6b5469172bce8e2e3e2a7215bc24a581ddc
Hamody <ahmedeslam229200@gmail.com>
2025-09-24 23:58:09 +0300

    Revert "Revert modified test.py with publisher"
    This reverts commit b6c9563b895d7c20bb820fd8a65e1d430f192fd7.

--- test.py
+++ test.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String  #MODIFY TYPE
-
-class PublisherNode(Node): #MODIFY NAME
+ 
+ 
+class MyCustomNode(Node): # MODIFY NAME
     def __init__(self):
-        super().__init__('minimal_publisher')
-
-        self.publisher_ = self.create_publisher(String, 'topic', 10) # MODIFY TOPIC AND TYPE
-
-        timer_period = 1.0  #MODIFY TIMER RATE 
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-
-    def timer_callback(self):
-        msg = String()
-        msg.data = f'Hello ROS2: {self.i}'
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: "{msg.data}"')
-        self.i += 1
-
+        super().__init__("node_name") # MODIFY NAME
+ 
+ 
 def main(args=None):
     rclpy.init(args=args)
-    node = PublisherNode()
+    node = MyCustomNode() # MODIFY NAME
     rclpy.spin(node)
     rclpy.shutdown()
-
-if __name__ == '__main__':
+ 
+ 
+if __name__ == "__main__":
     main()
